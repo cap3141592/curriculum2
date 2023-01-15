a354ec23f7daffca47a02d1782a5fe553bd918c7
cap3141592 <121149436+cap3141592@users.noreply.github.com>
2023-01-15 10:41:13 +0900

    4章バグ修正test2

---
+++ PHPjob/4-1/bugTest/test2/dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+// セッション開始
+// session_start();
+
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "Na2718281828";
+// データベース名
+$db['dbname'] = "yigroupBlog";
